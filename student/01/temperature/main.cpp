a1645ba83a72e95732a825529df509dda35724bf
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-09-03 13:39:15 +0300

    (N) Lämpötiloja, muunnos Fahrenheit-asteista Celsius-asteiksi

--- student/01/temperature/main.cpp
+++ student/01/temperature/main.cpp
@@ -8,6 +8,6 @@ int main()
     double temp = 0;
     cin >> temp;
     cout << temp << " degrees Celsius is " <<  temp * 1.8 + 32 << " degrees Fahrenheit" << endl;
-
+    cout << temp << " degrees Fahrenheit is " << (temp - 32) / 1.8 << " degrees Celsius" << endl;
     return 0;
 }
