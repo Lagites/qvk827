a7119074aa730d2d961eead54c8a54d4eb69d05d
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-09-03 13:52:22 +0300

    Lisätty myös 7:llä jaollisten lukujen käsittely

--- student/01/number_series_game/main.cpp
+++ student/01/number_series_game/main.cpp
@@ -7,8 +7,12 @@ int main()
     int numbers;
     std::cin >> numbers;
     for(int i = 1; i <= numbers; i++){
-        if(i % 3 == 0)
+        if(i % 3 == 0 && i % 7 == 0)
+            std::cout << "zip boing" << std::endl;
+        else if(i % 3 == 0)
             std::cout << "zip" << std::endl;
+        else if(i % 7 == 0)
+            std::cout << "boing" << std::endl;
         else
             std::cout << i << std::endl;
     }
