c59770deeea7de4b6b721bac891870023a9df6b6
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-09-03 19:37:50 +0300

    (N) Läheisimmät tekijät, Incrementoiva ratkaisu

--- student/01/factors/main.cpp
+++ student/01/factors/main.cpp
@@ -5,8 +5,29 @@ using namespace std;
 int main()
 {
     cout << "Enter a positive number: ";
-
-    // Write your code here
+    int number;
+    cin >> number;
+    if(number < 1){
+        cout << "Only positive numbers accepted" <<  endl;
+        return 0;
+    }
+    int firstResult = number;
+    int secondResult = 1;
+    int i = 1;
+    int j = number;
+    while(i <= j){
+        if(j * i > number){
+            j--;
+            i = 1;
+            continue;
+        }
+        if(j * i == number){
+            firstResult = i;
+            secondResult = j;
+        }
+        i++;
+    }
+    cout << number << " = " << firstResult << " * " << secondResult <<  endl;
 
     return 0;
 }
