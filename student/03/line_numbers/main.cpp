a08cdae125a187dfce432997f8dcccbd4522c7b4
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-09-11 10:47:07 +0300

    (N) Tiedoston rivien numerointi, luodaan kirjoitustiedosto vasta kun lukutiedosto on avattu

--- student/03/line_numbers/main.cpp
+++ student/03/line_numbers/main.cpp
@@ -14,29 +14,21 @@ int main()
     cin >> outputFile;
 
     ifstream reader(inputFile);
-    ofstream writer(outputFile);
     if(!reader){
         cout << "Error! The file " << inputFile << " cannot be opened." << endl;
         return EXIT_FAILURE;
     }
-
     else{
-        if(!writer){
-            cout << "Error! The file " << outputFile << " cannot be opened." << endl;
-            return EXIT_FAILURE;
+        ofstream writer(outputFile);
+        string line;
+        int i = 1;
+        while(getline(reader,line)){
+            writer << i << " " << line << endl;
+            i++;
         }
-        else{
-            string line;
-            int i = 1;
-            while(getline(reader,line)){
-                writer << i << " " << line << endl;
-                i++;
-            }
-        }
-
+        reader.close();
+        writer.close();
     }
-    reader.close();
-    writer.close();
 
     return EXIT_SUCCESS;
 }
