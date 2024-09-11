00a8879d5449e4a3202beb4e23d9bcfaef4dfb62
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-09-11 10:29:50 +0300

    (N) Tiedoston rivien numerointi, tiedoston luku ja kirjoitus

---
+++ student/03/line_numbers/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string inputFile;
+    string outputFile;
+    cout << "Input file: ";
+    cin >> inputFile;
+    cout << "Output file: ";
+    cin >> outputFile;
+
+    ifstream reader(inputFile);
+    ofstream writer(outputFile);
+    if(!reader)
+        cout << "Error! The file " << inputFile << " cannot be opened." << endl;
+    else{
+        if(!writer)
+            cout << "Error! The file " << outputFile << " cannot be opened." << endl;
+        else{
+            string line;
+            while(getline(reader,line)){
+                writer << line << endl;
+            }
+        }
+
+    }
+    reader.close();
+    writer.close();
+
+    return 0;
+}
