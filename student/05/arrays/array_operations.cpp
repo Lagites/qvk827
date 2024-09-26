250c2656dad7f12c06aaf599afd75fef8217a827
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-09-26 21:12:53 +0300

    Taulukko-operaatioita, operaatioiden toteutus

---
+++ student/05/arrays/array_operations.cpp
@@ -0,0 +1,46 @@
+#include "array_operations.hh"
+#include <iostream>
+
+int greatest_v1(int *itemptr, int size)
+{
+    int greatest = 0;
+    for(int i = 0 ; i < size ; i++){        
+        if(*itemptr > greatest)
+            greatest = *itemptr;
+        itemptr++;
+    }
+
+    return greatest;
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int greatest = 0;
+    while(itemptr != endptr){
+        if(*itemptr > greatest)
+            greatest = *itemptr;
+        itemptr++;
+    }
+    return greatest;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    while(itemptr != endptr){
+        *targetptr = *itemptr;
+        itemptr++;
+        targetptr++;
+    }
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+    while(leftptr < rightptr){
+        rightptr--;
+        int temp = *leftptr;
+        *leftptr = *rightptr;
+        *rightptr = temp;
+        leftptr++;
+        std::cout << "leftpt " << leftptr << " right " << rightptr << std::endl;
+    }
+}
