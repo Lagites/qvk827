1a04d2bfe3fb13341aa1f0c2b5ad0caef70eed9b
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-10-06 22:10:26 +0300

    Korttien siirtelyä, kortin lisäys, poisto ja tulostus

---
+++ student/06/cards/cards.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "cards.hh"
+#include <memory>
+
+using namespace std;
+
+Cards::Cards(): top_(nullptr)
+{
+    std::cout << "constructor" << std::endl;
+}
+
+Cards::~Cards()
+{
+    std::cout << "destructor" << std::endl;
+}
+
+void Cards::add(int id)
+{
+    shared_ptr<Card_data> new_ptr
+            = make_shared<Card_data>(Card_data{id, nullptr});
+
+    if ( top_ == nullptr ) {
+        top_ = new_ptr;
+    } else {
+        new_ptr->next = top_;
+        top_ = new_ptr;
+    }
+}
+
+bool Cards::remove(int &id)
+{
+    if( top_ == nullptr)
+            return false;
+    id = top_->data;
+    top_ = top_->next;
+}
+
+void Cards::print_from_top_to_bottom(std::ostream &s)
+{
+    shared_ptr<Card_data> printable_ptr = top_;
+    int running_number = 1;
+
+    while ( printable_ptr != nullptr ) {
+        cout << running_number << ". "
+             << printable_ptr->data << endl;
+        ++running_number;
+        printable_ptr = printable_ptr->next;
+    }
+}
+
+void Cards::print_from_bottom_to_top(std::ostream &s)
+{
+
+}
+
+bool Cards::top_to_bottom()
+{
+
+}
+
+void Cards::reverse()
+{
+
+}
+
+int Cards::recursive_print(std::shared_ptr<Card_data> top, std::ostream &s)
+{
+
+}
