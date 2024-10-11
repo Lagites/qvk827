2c237d4e4d7d03915970b0a2a27aff163a785664
Jukka Välimäki <jukka.valimaki@tuni.fi>
2024-10-12 00:00:02 +0300

    Suku on pahin, sukupuun muodostaminen ja lasten listaus

--- student/07/family/familytree.cpp
+++ student/07/family/familytree.cpp
@@ -11,12 +11,29 @@ void Familytree::addNewPerson(const std::string &id, int height, std::ostream &o
     shared_ptr<Person> new_ptr
             = make_shared<Person>(Person{id,height, {}, {}});
 
+    if(persons.find(id) != persons.end()){
+        output << "Error. " << id <<" already added." << endl;
+        return;
+    }
     persons[new_ptr->id_] = new_ptr;
 }
 
 void Familytree::addRelation(const std::string &child, const std::vector<std::string> &parents, std::ostream &output)
 {
-
+    auto iter = persons.find(child);
+    if(iter != persons.end()){
+        int i = 0;
+        for(const string &parent : parents){
+            auto iter2 = persons.find(parent);
+            if(iter2 != persons.end()){
+                iter->second->parents_.assign(i,iter2->second.get());
+                iter2->second->children_.push_back(iter->second.get());
+            }
+        }
+    }
+    else{
+        printNotFound(child,output);
+    }
 }
 
 void Familytree::printPersons(Params, std::ostream &output) const
@@ -30,7 +47,19 @@ void Familytree::printPersons(Params, std::ostream &output) const
 
 void Familytree::printChildren(Params params, std::ostream &output) const
 {
-
+    string id = params[0];
+    IdSet children = {};
+    auto iter = persons.find(id);
+    if(iter == persons.end()){
+        printNotFound(id,output);
+        return;
+    }
+    else{
+        for(Person* child : iter->second->children_){
+            children.insert(child->id_);
+        }
+    }
+    printGroup(id, "children", children, output);
 }
 
 void Familytree::printParents(Params params, std::ostream &output) const
@@ -70,20 +99,33 @@ void Familytree::printGrandParentsN(Params params, std::ostream &output) const
 
 Person *Familytree::getPointer(const std::string &id) const
 {
-
+    return persons.find(id)->second.get();
 }
 
 void Familytree::printNotFound(const std::string &id, std::ostream &output) const
 {
-
+   output << "Error. " << id <<" not found." << endl;
 }
 
 IdSet Familytree::vectorToIdSet(const std::vector<Person *> &container) const
 {
-
+    IdSet set;
+    for(Person *person : container){
+        set.insert(person->id_);
+    }
+    return set;
 }
 
 void Familytree::printGroup(const std::string &id, const std::string &group, const IdSet &container, std::ostream &output) const
 {
+    if(container.size() == 0){
+        output << id << " has no " << group << endl;
+        return;
+    }
 
+    output << id << " has " << container.size() << " " << group << ":" << endl;
+
+    for(string content : container){
+        output << content << endl;
+    }
 }
